e708c718053be05706c3d9c5d5a7cc206284f123
Wren Macayan <macayanwren@gmail.com>
2025-05-25 04:01:43 +0800

    feat: add new project route tracker

--- data/projects.js
+++ data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     description: 'Simple game called dice game was created using JavaScript, HTML, and CSS. You should play this game if you enjoy gambling. Attempt your luck!',
     url: 'https://dice-game-ten-ruby.vercel.app',
     tools: ['JavaScript', 'HTML', 'CSS'],
-    repo: 'https://github.com/yourusername/dice-game'
+    repo: 'https://github.com/hnddrx/RollTwoDice'
   },
   {
     icon: '🚀',
@@ -13,7 +13,7 @@ const projects = [
     description: 'Anoni Blogs is a blogging platform designed for people who find it difficult to open up to others since it allows you to express yourself without worrying about privacy.',
     url: 'https://anoni-blogs.vercel.app',
     tools: ['JavaScript', 'HTML', 'CSS', 'Node.js', 'React'],
-    repo: 'https://github.com/yourusername/anoni-blogs'
+    repo: 'https://github.com/hnddrx/AnoniBlogs'
   },
   {
     icon: '🧠',
@@ -21,7 +21,7 @@ const projects = [
     description: 'You can schedule a meeting with your psychiatrist using the appointment scheduling service Lean On.',
     url: 'https://lean-on.vercel.app',
     tools: ['JavaScript', 'HTML', 'CSS', 'Node.js', 'React', 'Express', 'MongoDB'],
-    repo: 'https://github.com/yourusername/lean-on'
+    repo: 'https://github.com/hnddrx/LeanOn'
   },
   {
     icon: '📊',
@@ -46,6 +46,14 @@ const projects = [
     url: 'https://wren-portfolio.vercel.app/taskmanager',
     tools: ['Python', 'Odoo', 'PostgreSQL', 'XML', 'JavaScript'],
     repo: 'https://github.com/yourusername/hrms-app'
+  },
+  {
+    icon: '🏢',
+    title: 'Route Planner',
+    description: ' Route Planner with Live Traffic Insight Plan smarter journeys with the Route Planner App, an interactive mapping tool designed to help you find optimal travel routes in the Philippines. Powered by OpenRouteService and HERE Traffic API, this app allows users to:',
+    url: 'https://eta-tracker-iota.vercel.app/',
+    tools: ['Python', 'Odoo', 'PostgreSQL', 'XML', 'JavaScript'],
+    repo: 'https://github.com/hnddrx/router_planner'
   },
 ];
 
