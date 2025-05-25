26c539f308c25ac2dc19eccce3d81533d556d467
Wren Macayan <macayanwren@gmail.com>
2025-05-25 10:04:45 +0800

    fix: icon for project route

--- data/projects.js
+++ data/projects.js
@@ -48,13 +48,14 @@ const projects = [
     repo: 'https://github.com/yourusername/hrms-app'
   },
   {
-    icon: '🏢',
-    title: 'Route Planner',
-    description: ' Route Planner with Live Traffic Insight Plan smarter journeys with the Route Planner App, an interactive mapping tool designed to help you find optimal travel routes in the Philippines. Powered by OpenRouteService and HERE Traffic API, this app allows users to:',
-    url: 'https://eta-tracker-iota.vercel.app/',
-    tools: ['Python', 'Odoo', 'PostgreSQL', 'XML', 'JavaScript'],
-    repo: 'https://github.com/hnddrx/router_planner'
-  },
+    "icon": "🗺️",
+    "title": "Route Planner",
+    "description": "Route Planner with Live Traffic Insight. Plan smarter journeys with the Route Planner App, an interactive mapping tool designed to help you find optimal travel routes in the Philippines. Powered by OpenRouteService and HERE Traffic API, this app allows users to:",
+    "url": "https://eta-tracker-iota.vercel.app/",
+    "tools": ["Python", "Odoo", "PostgreSQL", "XML", "JavaScript"],
+    "repo": "https://github.com/hnddrx/router_planner"
+  }
+  
 ];
 
 export default projects;
